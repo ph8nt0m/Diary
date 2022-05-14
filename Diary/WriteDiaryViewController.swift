4d87ed53bce6eb7f5fef6ddd0c7cc3823d67893c
ph8nt0m <phant0m@kakao.com>
2022-05-14 20:59:01 +0900

    feat: Write Diary Page UI

---
+++ Diary/WriteDiaryViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WriteDiaryViewController.swift
+//  Diary
+//
+//  Created by 이동준 on 2022/05/14.
+//
+
+import UIKit
+
+class WriteDiaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
