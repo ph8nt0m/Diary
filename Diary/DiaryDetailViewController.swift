f1c98f179bc4cdc2b81cd2272470af0d819d82f7
ph8nt0m <phant0m@kakao.com>
2022-05-16 10:36:49 +0900

    feat: Diary Detail UI

---
+++ Diary/DiaryDetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DiaryDetailViewController.swift
+//  Diary
+//
+//  Created by 이동준 on 2022/05/16.
+//
+
+import UIKit
+
+class DiaryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
